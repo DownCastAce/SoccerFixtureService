5a8220a6c974a1160fc1e0460a0127786b37b19d
lorcan <lorcan.lod@gmail.com>
2014-11-19 22:53:49 +0000

    added new tests

--- testPage.py
+++ testPage.py
@@ -1,19 +1,37 @@
 from bs4 import BeautifulSoup
 
 def testPageGet():
-	f = open("tests/arsenalHardCopy/first-team", "r")
-        content = f.read()
+	f = open("tests/first-team", "r")
+	content = f.read()
 	assert content != None
 
 def testBS():
-	f = open("tests/arsenalHardCopy/first-team", "r")
+	f = open("tests/first-team", "r")
 	content = f.read()
 	print content[:10]
 	soup = BeautifulSoup(content)
-        assert soup != None
-        for elem in soup.find_all(class_='versus'):
+	assert soup != None
+	for elem in soup.find_all(class_='versus'):
+		print elem
+
+def testNextTeam():
+	f = open("tests/first-team", "r")
+	content = f.read()
+	soup = BeautifulSoup(content)
+	assert soup != None
+	for elem in soup.find_all(class_='fixture-info'):
+		print elem
+
+def testNextTeamInfo():
+	f = open("tests/first-team", "r")
+	content = f.read()
+	soup = BeautifulSoup(content)
+	assert soup != None
+	for elem in soup.find_all(class_='fixture-info'):
 		print elem
 
 if __name__ == "__main__":
 	testPageGet()
- 	testBS()
+#	testBS()
+#	testNextTeam()
+	testNextTeamInfo()
